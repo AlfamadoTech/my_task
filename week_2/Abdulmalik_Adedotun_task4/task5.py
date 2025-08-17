a10bf20eea8cbb8769a850db9b27ac529e7d4671
Abdulmalik <abdulmalikadedotun@gmail.com>
2025-08-17 23:26:58 +0300

    assignment

--- week_2/Abdulmalik_Adedotun_task4/task5.py
+++ week_2/Abdulmalik_Adedotun_task4/task5.py
@@ -1,12 +1,11 @@
+# Student score tracker
 students_name = []
 students_score = []
-student_name = input("Enter the three student name: ")
+student_name = input("Enter the three student name(seperated by a comma): ").split(",")
 students_name.append(student_name)
-student_score = input("Enter the student score: ")
+student_score = input("Enter the student scores(sererated by a comma): ").split(",")
 students_score.append(student_score)
-for i in students_name, students_score:
-    if len(students_name) == len(students_score):
-        students_name[i] = students_score[i]
-        print(f"{students_name} - {students_score}")
 
-
+print("--------Student Name and Score--------")
+print("student_name\t\t\tstudent_score")
+print(f"{students_name}\t\t{students_score}")
