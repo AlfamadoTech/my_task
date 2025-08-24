2f0bb80c9a0151d6eba1e20301337b4f7bcccca7
Abdulmalik <abdulmalikadedotun@gmail.com>
2025-08-24 23:57:36 +0300

    weekend work

--- week_3/Abdulmalik_Adedotun_task8/task2.py
+++ week_3/Abdulmalik_Adedotun_task8/task2.py
@@ -3,15 +3,23 @@ citizen_name = input("Enter your name: ").title()
 citizen_age = int(input("Enter your age: "))
 citizen_score = int(input("Enter your test score: "))
 citizenship = input("Are you a citizen of Nigeria (Yes/No): ").lower()
-Enrollment_status = input("Are you a full-time undegraduate student in a recognised university (Yes/No): ").lower()
-other_scholarship = input("are you a beneficiary of any other scholarship from oil and gas industry both local and foreign (Yes/No): ").lower()
-if (citizenship == "yes") and (Enrollment_status == "yes") and (other_scholarship == "no"):
-    print("He is a citizen, enrolled full-time in recognised Nigeria University and isn't a beneficiary of any oil ang gas scholarship, checking age and score, WAEC?WASSCE grades for eligibility of scholarship")
-    # waec_wassce_result = input(f"Enter waec results grades in 5 key subject including math and english, must be (A/B): ").split(", ")
-    waec_subject = input("Enter waec key waec/wassce subject including math and english: ").split()
-    waec_subject_grade = input("Enter waec grades").split()
-    waec_wassce_result = dict(zip(waec_subject, waec_subject_grade))
-    print(waec_wassce_result)
-    if waec_subject_grade == "A" or "B":
-        eligibility_criteria = (citizen_age < 18) and (citizen_score > 70)
-        print(f"Candidate: {citizen_name}\nAge: {citizen_age}\nScore: {citizen_score}\nEligible: {eligibility_criteria}")
+enrollment_status = input("Are you a full-time undergraduate student in a recognised university (Yes/No): ").lower()
+other_scholarship = input("Are you a beneficiary of any other scholarship from oil and gas industry both local and foreign (Yes/No): ").lower()
+
+waec_subject = input("Enter WAEC subjects (including Math and English, space separated): ").upper().split(",")
+waec_subject_grade = input("Enter WAEC grades (space separated): ").upper().split(",")
+waec_wassce_result = dict(zip(waec_subject, waec_subject_grade))
+print(f"WAEC Result: {waec_wassce_result}")
+
+basic_check = (citizenship == "yes") and (enrollment_status == "yes") and (other_scholarship == "no")
+
+waec_check = set(waec_subject_grade).issubset({"A", "B"})
+
+
+eligibility_criteria = basic_check and waec_check and (citizen_age < 18) and (citizen_score > 70)
+
+messages = {
+    True: f"Candidate: {citizen_name}\nAge: {citizen_age}\nScore: {citizen_score}\nEligible: True",
+    False: f"Candidate: {citizen_name}\nNot Eligible for Scholarship"
+}
+print(messages[eligibility_criteria])
