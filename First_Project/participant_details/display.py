d1a0f306b4c6d5780889664256737701ba848a08
Abdulmalik <abdulmalikadedotun@gmail.com>
2025-09-10 15:46:21 +0100

    all task

---
+++ First_Project/participant_details/display.py
@@ -0,0 +1,11 @@
+# Simple formatting and displaying
+
+def show_menu():
+    print("\n=== Phone Contact Manager ===")
+    print("1. Create Contact")
+    print("2. Update Contact")
+    print("3. Delete Contact")
+    print("4. Read Contacts")
+    print("5. Search Contact")
+    print("6. Save to CSV")
+    print("7. Exit")
